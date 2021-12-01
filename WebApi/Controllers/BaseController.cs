a7cbef55b81f7aa936417f935b1270fc46d53179
Luong Dang <dangvluong@outlook.com>
2021-12-01 12:02:11 +0700

    Add basic controllers

---
+++ WebApi/Controllers/BaseController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApi.Models;
+
+namespace WebApi.Controllers
+{
+    public class BaseController : ControllerBase
+    {
+        protected readonly AppDbContext _context;
+
+        public BaseController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+    }
+}
