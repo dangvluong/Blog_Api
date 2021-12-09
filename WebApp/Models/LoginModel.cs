778978221551c22bfedaaa0400fed54136cac492
Luong Dang <dangvluong@outlook.com>
2021-12-09 10:02:29 +0700

    Edit UI

--- WebApp/Models/LoginModel.cs
+++ WebApp/Models/LoginModel.cs
@@ -6,11 +6,14 @@ namespace WebApp.Models
     {
         [Required]
         [StringLength(32, MinimumLength = 6)]
+        [Display(Name = "Tên đăng nhập")]
         public string Username { get; set; }
         [Required]
         [StringLength(64, MinimumLength = 6)]
         [DataType(DataType.Password)]
+        [Display(Name = "Mật khẩu")]
         public string Password { get; set; }
+        [Display(Name = "Ghi nhớ tài khoản")]
         public bool Remember { get; set; }
     }
 }
--- WebApp/Models/RegisterModel.cs
+++ WebApp/Models/RegisterModel.cs
@@ -6,19 +6,25 @@ namespace WebApp.Models
     {
         [Required]
         [StringLength(32, MinimumLength = 6)]
+        [Display(Name = "Tên tài khoản")]
         public string Username { get; set; }
         [Required]
         [StringLength(64, MinimumLength = 6)]
         [DataType(DataType.Password)]
+        [Display(Name = "Mật khẩu")]
         public string Password { get; set; }
         [Required]
+        [Display(Name = "Giới tính")]
         public bool Gender { get; set; }
         [Required]
+        [Display(Name = "Tên đầy đủ")]
         public string FullName { get; set; }
         [Required]
         [EmailAddress]
+        [Display(Name = "Địa chỉ email")]
         public string Email { get; set; }
         [DataType(DataType.Date)]
+        [Display(Name = "Ngày sinh")]
         public DateTime DateOfBirth { get; set; }        
     }
 }
