519371b79d391bb8709daf023dba75334cf54134
LuongDV <dangvluong@gmail.com>
2022-03-12 10:42:10 +0700

    Fix bug send email from Azure: create app password on gmail account

--- WebApp/Services/MailServices.cs
+++ WebApp/Services/MailServices.cs
@@ -24,21 +24,18 @@ namespace WebApp.Services
                 message.IsBodyHtml = true;
                 message.Body = $"Vui lòng click vào <a href=\"https://localhost:7211/account/resetpassword?email={addressTo}&token={resetPasswordToken}\">ĐÂY</a> để thiết lập lại mật khẩu của bạn. ";
                 message.Subject = "CẬP NHẬT MẬT KHẨU";
-                using (SmtpClient client = new SmtpClient(section["host"], int.Parse(section["port"]))
+                using SmtpClient client = new SmtpClient(section["host"], int.Parse(section["port"]));
+                client.UseDefaultCredentials = false;
+                client.Credentials = new NetworkCredential(section["address"], section["password"]);
+                client.EnableSsl = true;                
+                try
                 {
-                    Credentials = new NetworkCredential(section["address"], section["password"]),
-                    EnableSsl = true
-                })
+                    logger.LogInformation($"Send email reset password to {addressTo}");
+                    await client.SendMailAsync(message);
+                }
+                catch (Exception ex)
                 {
-                    try
-                    {
-                        logger.LogInformation($"Send email reset password to {addressTo}");
-                        await client.SendMailAsync(message);
-                    }
-                    catch (Exception ex)
-                    {
-                        logger.LogError(ex, "Unhandle exception");
-                    }
+                    logger.LogError(ex, "Unhandle exception");
                 }
             }
         }
