8de8629b94c5becef37d484627b2d1faf93cdd5c
LuongDV <dangvluong@gmail.com>
2022-03-16 13:40:39 +0700

    Edit PaginationTagHelper

--- WebApp/Helper/PaginationTagHelper.cs
+++ WebApp/Helper/PaginationTagHelper.cs
@@ -14,39 +14,85 @@ namespace WebApp.Helper
             output.Attributes.Add("class", "pagination-wrapper mt-4");
             StringBuilder sb = new StringBuilder();
             sb.Append("<div class=\"page-pagination\"><ul class=\"page-numbers\">");
-            if (CurrentPage == null)
+            if (TotalPage <= 5)
             {
-                string uri = string.Format(Url, "");                
-                sb.AppendFormat("<li><span aria-current=\"page\" class=\"page-numbers current\">{0}</span></li>", 1);
-                for (int i = 2; i <= TotalPage; i++)
+                if (CurrentPage == null)
                 {
-                    uri = string.Format(Url, $"page={i}");
-                    sb.AppendFormat("<li><a class=\"page-numbers\" href=\"{1}\">{0}</a></li>", i, uri);
+                    string uri = string.Format(Url, "");
+                    sb.AppendFormat("<li><span aria-current=\"page\" class=\"page-numbers current\">{0}</span></li>", 1);
+                    for (int i = 2; i <= TotalPage; i++)
+                    {
+                        uri = string.Format(Url, $"page={i}");
+                        sb.AppendFormat("<li><a class=\"page-numbers\" href=\"{1}\">{0}</a></li>", i, uri);
+                    }
+                    if (TotalPage > 1)
+                        AddNagivation(isNext: true, sb, targetPage: 2);
                 }
-                if (TotalPage > 1)
-                    AddNagivation(isNext: true,sb,targetPage: 2);
+                else
+                {
+                    int currentPage = Convert.ToInt32(CurrentPage);
+                    AddNagivation(isNext: false, sb, targetPage: currentPage - 1);
+                    string uri = string.Format(Url, "");
+                    sb.AppendFormat("<li><a class=\"page-numbers\" href=\"{1}\">{0}</a></li>", 1, uri);
+                    for (int i = 2; i <= TotalPage; i++)
+                    {
+                        uri = string.Format(Url, $"page={i}");
+                        if (currentPage == i)
+                        {
+                            sb.AppendFormat("<li><span aria-current=\"page\" class=\"page-numbers current\">{0}</span></li>", i, uri);
+                        }
+                        else
+                        {
+                            sb.AppendFormat("<li><a class=\"page-numbers\" href=\"{1}\">{0}</a></li>", i, uri);
+                        }
+                    }
+                    if (currentPage < TotalPage)
+                        AddNagivation(isNext: true, sb, targetPage: currentPage + 1);
+                }
+
             }
             else
             {
-                int currentPage = Convert.ToInt32(CurrentPage);
-                if (currentPage > 1)
-                    AddNagivation(isNext: false, sb, targetPage:currentPage - 1);
-                string uri = string.Format(Url, "");
-                sb.AppendFormat("<li><a class=\"page-numbers\" href=\"{1}\">{0}</a></li>", 1, uri);
-                for (int i = 2; i <= TotalPage; i++)
+                if (CurrentPage == null)
                 {
-                    uri = string.Format(Url, $"page={i}");
-                    if (currentPage == i)
+                    string uri = string.Format(Url, "");
+                    sb.AppendFormat("<li><span aria-current=\"page\" class=\"page-numbers current\">{0}</span></li>", 1);
+                    for (int i = 2; i <= 5; i++)
                     {
-                        sb.AppendFormat("<li><span aria-current=\"page\" class=\"page-numbers current\">{0}</span></li>", i, uri);
-                    }
-                    else
-                    {
+                        uri = string.Format(Url, $"page={i}");
                         sb.AppendFormat("<li><a class=\"page-numbers\" href=\"{1}\">{0}</a></li>", i, uri);
                     }
+                    AddNagivation(isNext: true, sb, targetPage: 2);
                 }
-                if (currentPage < TotalPage)
-                    AddNagivation(isNext: true,sb, targetPage:currentPage + 1);
+                else
+                {
+                    int currentPage = Convert.ToInt32(CurrentPage);                   
+                    AddNagivation(isNext: false, sb, targetPage: currentPage - 1);
+                    int j = currentPage;
+                    if (currentPage - 1 == 0)
+                        j += 2;
+                    if (currentPage - 2 == 0)
+                        j++;
+                    if (TotalPage - currentPage == 1)
+                        j -= 1;
+                    if (TotalPage - currentPage == 0)
+                        j -= 2;
+
+                    for (int i = j - 2; i <= j + 2; i++)
+                    {
+                        string uri = string.Format(Url, $"page={i}");
+                        if (currentPage == i)
+                        {
+                            sb.AppendFormat("<li><span aria-current=\"page\" class=\"page-numbers current\">{0}</span></li>", i, uri);
+                        }
+                        else
+                        {
+                            sb.AppendFormat("<li><a class=\"page-numbers\" href=\"{1}\">{0}</a></li>", i, uri);
+                        }
+                    }
+                    if (currentPage < TotalPage)
+                        AddNagivation(isNext: true, sb, targetPage: currentPage + 1);
+                }
             }
             output.Content.SetHtmlContent(sb.ToString());
         }
@@ -56,6 +102,6 @@ namespace WebApp.Helper
             string uri = string.Format(Url, $"page={targetPage}");
             string direction = isNext ? "Next" : "Prev";
             sb.AppendFormat($"<li><a class=\"{direction} page-numbers\" href=\"{uri}\">{direction}</a></li>");
-        }       
+        }
     }
 }
